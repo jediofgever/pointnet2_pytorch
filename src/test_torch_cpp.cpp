01abadcb8399b9c0f47917aa5fe6d59056b3d587
jediofgever <fetulahatas1@gmail.com>
2021-04-17 11:03:01 +0800

    test FPS

--- src/test_torch_cpp.cpp
+++ src/test_torch_cpp.cpp
@@ -57,6 +57,26 @@ at::Tensor furthest_point_sampling(at::Tensor * input_tensor, int num_samples, b
   return centroids;
 }
 
+at::Tensor extract_tensor_from_indices(at::Tensor * input_indices, at::Tensor * input_tensor)
+{
+  // This indices are usally
+  c10::IntArrayRef input_shape = input_indices->sizes();
+  at::Tensor extracted_tensor = at::zeros(
+    {input_shape[0],
+      input_shape[1], 3},
+    input_indices->device());
+
+  for (int i = 0; i < input_shape[0]; i++) {
+    for (int j = 0; j < input_shape[1]; j++) {
+      pcl::PointXYZRGB crr_point;
+      int index = input_indices->index({i, j}).item<int>();
+      at::Tensor sampled_point = input_tensor->index({i, index});
+      extracted_tensor.index_put_({i}, sampled_point);
+    }
+  }
+  return extracted_tensor;
+}
+
 /**
  * @brief checks whether cuda is avail for use
  *
@@ -83,7 +103,6 @@ void torch_tensor_to_pcl_cloud(
       crr_point.z = input_tensor->index({i, j, 2}).item<float>() * 10.0;
       crr_point.g = 1.0;
       cloud->points.push_back(crr_point);
-      std::cout << crr_point.x << std::endl;
     }
   }
   cloud->width = 1;
@@ -96,16 +115,26 @@ int main()
   test_if_cuda_avail();
 
   // to test FPS(Furthest-point-sampleing algorithm)
-  c10::IntArrayRef test_tensor_shape = {90, 4, 3};
+  c10::IntArrayRef test_tensor_shape = {4, 4, 3};
   torch::Device cuda_device = torch::kCUDA;
   at::Tensor test_tensor = at::rand(test_tensor_shape, cuda_device);
 
-  furthest_point_sampling(&test_tensor, 1, true);
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-  torch_tensor_to_pcl_cloud(&test_tensor, cloud);
-  std::cout << "Converted a torch tensor to pcl cloud size is: " << cloud->points.size();
+  at::Tensor fps_sampled_tensor_indices = furthest_point_sampling(&test_tensor, 1, false);
+  at::Tensor fps_sampled_tensor = extract_tensor_from_indices(
+    &fps_sampled_tensor_indices,
+    &test_tensor);
 
-  pcl::io::savePCDFile("../data/rand.pcd", *cloud, false);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr full_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr fps_sampled_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+  std::cout << "test_tensor: \n" << test_tensor << std::endl;
+  std::cout << "fps_sampled_tensor_indices: \n" << fps_sampled_tensor_indices << std::endl;
+  std::cout << "fps_sampled_tensor:  \n" << fps_sampled_tensor << std::endl;
+
+  torch_tensor_to_pcl_cloud(&test_tensor, full_cloud);
+  torch_tensor_to_pcl_cloud(&fps_sampled_tensor, full_cloud);
+
 
   return EXIT_SUCCESS;
+
 }
